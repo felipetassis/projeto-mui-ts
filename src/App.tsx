ade2b0d80412ec55ed3454abb98acef705e746c1
Felipe Tassis <felipe.detassis@gmail.com>
2023-06-12 16:07:15 -0300

    feat: Configurando corretamento EsLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
-
 import { AppThemeProvider } from './shared/contexts';
 import { AppRoutes } from  './routes';
+import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
+
+        <MenuLateral />
         <AppRoutes />
+        
       </BrowserRouter>
     </AppThemeProvider>
   );
